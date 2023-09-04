1380a93333d1ef0bf991893d462b2723e8208aa6
Joe Bell <7349341+joe-bell@users.noreply.github.com>
2023-09-04 17:03:29 +0300

    example commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+
+const includeFiles = [
+  "src/assets/images/hello-there.jpg",
+  ...Object.keys(import.meta.glob("./public/assets/fonts/*")),
+];
+
+// https://astro.build/config
+export default defineConfig({
+  output: "hybrid",
+  adapter: vercel({ functionPerRoute: false }),
+});
---
+++ src/pages/api/hello-there.ts
@@ -0,0 +1,54 @@
+import type { APIRoute } from "astro";
+import path from "node:path";
+import fs from "node:fs/promises";
+import sharp from "sharp";
+
+const helloThere = await sharp(
+  path.join(process.cwd(), "src", "assets", "images", "hello-there.jpg")
+)
+  .resize({ width: 48, height: 48, fit: "cover" })
+  .normalise()
+  .toBuffer({ resolveWithObject: true })
+  .then(
+    ({ data, info }) =>
+      `data:image/${info.format};base64,${data.toString("base64")}`
+  );
+
+const interRegular = await fs
+  .readFile(
+    path.join(process.cwd(), "public", "assets", "fonts", "Inter-Regular.woff")
+  )
+  .catch((err) => {
+    throw new Error(err);
+  })
+  .then(() => "interRegular exists!");
+
+const interBold = await fs
+  .readFile(
+    path.join(process.cwd(), "public", "assets", "fonts", "Inter-Bold.woff")
+  )
+  .catch((err) => {
+    throw new Error(err);
+  })
+  .then(() => "interBold exists!");
+
+export const GET: APIRoute = async () =>
+  new Response(
+    JSON.stringify(
+      {
+        body: {
+          helloThere,
+          interRegular,
+          interBold,
+        },
+      },
+      null,
+      2
+    ),
+    {
+      headers: {
+        "content-type": "text/plain",
+      },
+      status: 200,
+    }
+  );
